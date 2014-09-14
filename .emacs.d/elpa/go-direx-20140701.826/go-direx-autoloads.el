0233ee14ccdfec17bd003fbef9422560ca404ca2
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-09-14 15:35:17 +0200

    Add go plugins.

---
+++ .emacs.d/elpa/go-direx-20140701.826/go-direx-autoloads.el
@@ -0,0 +1,34 @@
+;;; go-direx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (go-direx-switch-to-buffer go-direx-pop-to-buffer)
+;;;;;;  "go-direx" "go-direx.el" (21525 38999 517470 81000))
+;;; Generated autoloads from go-direx.el
+
+(autoload 'go-direx-pop-to-buffer "go-direx" "\
+
+
+\(fn)" t nil)
+
+(autoload 'go-direx-switch-to-buffer "go-direx" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("go-direx-pkg.el") (21525 38999 522641
+;;;;;;  186000))
+
+;;;***
+
+(provide 'go-direx-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-direx-autoloads.el ends here
