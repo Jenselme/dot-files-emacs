6d4104c69e5c858c588cd49b4a2cebd461833e8b
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-10-02 10:03:10 +0200

    Add ac-dcd plugin.

--- .emacs.d/config-d.el
+++ .emacs.d/config-d.el
@@ -5,3 +5,13 @@
    1 2 nil (3 . 4)))
 (add-hook 'd-mode-hook 'flymake-mode)
 (add-hook 'd-mode-hook 'auto-complete-mode)
+
+(require 'ac-dcd)
+(add-to-list 'ac-modes 'd-mode)
+(add-hook 'd-mode-hook
+          (lambda () "set up ac-dcd"
+            (auto-complete-mode t)
+            (yas-minor-mode-on)
+            (ac-dcd-maybe-start-server)
+            (ac-dcd-add-imports)
+            (add-to-list 'ac-sources 'ac-source-dcd)))
