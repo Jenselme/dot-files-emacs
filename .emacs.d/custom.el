498d571567c2e99966309cc15006a0a738cd8711
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-09-07 19:03:04 +0200

    Update plugins

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40")
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (apache-mode yasnippet-bundle yasnippet yaml-mode web-mode w3m vala-mode textile-mode stylus-mode ssh-config-mode smex scala-mode2 sass-mode rust-mode python-mode pymacs pylint pyflakes php-mode pep8 org nginx-mode multi-web-mode markdown-mode key-chord jtags-extras jtags json-mode jinja2-mode jedi-direx javap-mode ipython ido-ubiquitous htmlize go-mode go-errcheck go-direx go-autocomplete git-commit-mode flymake-shell flymake-sass flymake-rust flymake-ruby flymake-python-pyflakes flymake-php flymake-jslint flymake-go flymake-d flymake-css flymake flycheck-tip flycheck-rust flycheck-pyflakes flycheck-haskell flycheck-d-unittest ess erlang emacs-eclim egg ecb doctags d-mode csv-mode creole-mode cpputils-cmake company color-theme cmake-mode cdlatex autopair auto-shell-command auto-complete-nxml auto-complete-clang auctex anything-show-completion anything-match-plugin anything-ipython anything-complete anything adoc-mode ac-python ac-js2 ac-html ac-dcd))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ .emacs.d/elpa/stylus-mode-0.1/stylus-mode-autoloads.el
@@ -0,0 +1,16 @@
+;;; stylus-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("stylus-mode.el") (22133 33116 497161
+;;;;;;  281000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; stylus-mode-autoloads.el ends here
